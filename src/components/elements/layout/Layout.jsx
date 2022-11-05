bf002616a56415d92f52529a38575c141c906319
DESKTOP-HCK0909\ggg71 <ggg7152@gmail.com>
2022-11-05 22:49:19 +0900

    ⭐ feat : 전체 레이아웃 구조 잡기

--- src/components/elements/layout/Layout.jsx
+++ src/components/elements/layout/Layout.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
 import Header from '../Header';
+import SideBar from '../SideBar';
 
 const Layout = () => {
   return (
-    <>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-    </>
+    <Wrapper>
+      <SideBar />
+      <MainWrapper>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+      </MainWrapper>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export default Layout;
